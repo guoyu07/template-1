1ee7d826001ab10d30bfbe2537f2cb3e981595b1
丁一鸣 <315129552@qq.com>
2016-10-11 23:41:49 +0800

    fix url-loader and file-loader

--- template/client/src/views/Home.vue
+++ template/client/src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
 	<div class="precss">
-		<h1>Home.vue</h1>
+		<h1>Powered By Vuejs 2</h1>
+		<img src="../assets/img/logo.png" alt="">
 	</div>
 </template>
 
 <style lang="css" scoped>
 .precss {
 	display: flex;
+	flex-direction: column;
+	align-items: center;
+	color: #41b883;
 }
 </style>
