74200c36bc4acb39794205ce140941a1d631d42d
丁一鸣 <315129552@qq.com>
2016-10-21 17:45:41 +0800

    update server.js for leancloud

--- template/server.js
+++ template/server.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const path = require('path')
-
-const PORT = 8080
 const app = express()
+const PORT = process.env.LEANCLOUD_APP_PORT || 8080
 const router = require('./server/router')
+const compression = require('compression')
 
+/*
+// leanengine add next
+var AV = require('leanengine')
+AV.init({
+  appId: process.env.LEANCLOUD_APP_ID,
+  appKey: process.env.LEANCLOUD_APP_KEY,
+  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
+})
+app.use(AV.express())
+*/
+
+app.use(compression())
 app.use(router)
 app.use(express.static(path.join(__dirname, 'public')))
 
