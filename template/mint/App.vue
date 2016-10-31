3b1feefb7148a8ed0adc1837e5b63154df90d353
dingyiming <315129552@qq.com>
2016-10-31 12:21:04 +0800

    add reset

--- template/mint/App.vue
+++ template/mint/App.vue
@@ -6,14 +6,25 @@
 
 
 <style lang="css">
+@import "./assets/css/reset.css";
 html {
 	font-size: 100px;
 }	
 body {
 	font-size: 16px;
+	background-color: #f8f8f8;
 }
 html, body {
 	width: 100%;
 	height: 100%;
+}
+.app-container {
+	height: 100%;
+}
+.page-scroll {
+	overflow: scroll;
+	height: 100%;
+	-webkit-overflow-scrolling: touch;
+	overflow-x: hidden;
 }
 </style>
