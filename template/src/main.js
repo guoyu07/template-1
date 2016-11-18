5bd9cab05a47e5267638a92a8ce720203cf18b26
dingyiming <315129552@qq.com>
2016-11-18 14:08:55 +0800

    add fastclick

--- template/src/main.js
+++ template/src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import MintUI from 'mint-ui';
+import fastclick from 'fastclick';
 import 'mint-ui/lib/style.css';
 import App from './App';
 import router from './router';
 
 Vue.use(MintUI);
+
+fastclick.attach(document.body);
 
 /* eslint-disable no-new */
 new Vue({
